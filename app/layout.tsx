c0f5d7076b2b113691094246d86617a5b0662b5c
deniel667 <deniel667@gmail.com>
2026-02-16 18:48:18 +0900

    Refine heading wrap and keep only Noto JP fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
-import { EB_Garamond, Kaisei_Tokumin, Montserrat, Noto_Sans_JP, Noto_Serif_JP, Zen_Old_Mincho } from 'next/font/google';
+import { Noto_Sans_JP, Noto_Serif_JP } from 'next/font/google';
 import './globals.css';
 
 const notoSans = Noto_Sans_JP({
@@ -11,30 +10,6 @@ const notoSans = Noto_Sans_JP({
 
 const notoSerif = Noto_Serif_JP({
   variable: '--font-noto-serif-jp',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-});
-
-const zenOldMincho = Zen_Old_Mincho({
-  variable: '--font-jp-display',
-  subsets: ['latin'],
-  weight: ['400', '500', '700', '900'],
-});
-
-const kaiseiTokumin = Kaisei_Tokumin({
-  variable: '--font-jp-alt',
-  subsets: ['latin'],
-  weight: ['400', '500', '700', '800'],
-});
-
-const ebGaramond = EB_Garamond({
-  variable: '--font-en',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-});
-
-const montserrat = Montserrat({
-  variable: '--font-montserrat',
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
 });
@@ -53,16 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <head>
-        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
-        <Script src="//typesquare.com/3/tsst/script/ja/typesquare.js?60cc056642f8410eb48d31d0e90393a3" strategy="beforeInteractive" />
-        <Script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?80f78LLNTUM%3D&box=LzwzK3bXsVs%3D&aa=1&ab=2" strategy="beforeInteractive" />
-      </head>
+      <head />
       <body
         data-rev={CACHE_REV}
-        className={`${notoSans.variable} ${notoSerif.variable} ${zenOldMincho.variable} ${kaiseiTokumin.variable} ${ebGaramond.variable} ${montserrat.variable} antialiased`}
+        className={`${notoSans.variable} ${notoSerif.variable} antialiased`}
       >
         {children}
       </body>
